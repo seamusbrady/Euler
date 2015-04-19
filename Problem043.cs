bf24609b02d77ddfa77d791a3c3ab0840065925f
seamusbrady <seamus.brady@gmail.com>
2015-04-19 21:35:36 +0100

    solve #43

--- Problem043.cs
+++ Problem043.cs
@@ -1,47 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
+using Euler.Utility;
 
 namespace Euler
 {
     internal class Problem043
     {
-        public BigInteger Run()
+        public long Run()
         {
-            
-            var primes = Utility.Prime.GetPrimesUsingSieveOfSundaram(1000);
+            return FastSolution();
+        }
 
-
-            int number = 406357289;
-            int len = (int) Math.Log10(number);
-            Console.WriteLine("len = {0}", len);
-
-            int min = 100000000;
-            int max = 999999999;
+        public long MySolution()
+        {
 
             int[] divisors = { 1, 2, 3, 5, 7, 11, 13, 17 };
 
-            for (int candidate = min; candidate <= max; candidate++)
-            {
-            }
+            const int len = 9;
 
-            for (int i = 0; i < len - 1; i++)
-            {
-                int power = (int) Math.Pow(10, len - 3 - i + 1);
+            long sumDividiblePanDigitals = 0;
+            var pandigitals =  PanDigital.GetAll();
 
-                int part = number/power;
-                int section = part % 1000;
-                if (primes.Contains(section))
+            foreach (var pandigital in pandigitals)
+            {
+                bool hasDivisors = true;
+                for (int i = len-2; i > 0; i--)
+                {
+                    int power = (int)Math.Pow(10, len - 3 - i + 1);
+
+                    var part = pandigital / power;
+                    var section = part % 1000;
+
+                    if (section%divisors[i] == 0) continue;
+                    hasDivisors = false;
                     break;
+                }
 
-                Console.WriteLine("i={3}: {0} / {1} = {2}, section = {4}, isPrime: {5}", number, power, part, i, section, primes.Contains(section));
+                if (!hasDivisors) continue;
 
+                sumDividiblePanDigitals += pandigital;
+                Console.WriteLine(pandigital);
             }
 
-
-
-            return 0;
+            return sumDividiblePanDigitals;
         }
 
+
+
+        public static long FastSolution()
+        {
+            int[] seq = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int[] primes = new int[] { 2, 3, 5, 7, 11, 13, 17 };
+
+            int[] limits = new int[seq.Length];
+            int count = 1;
+            for (int index = limits.Length - 1; index >= 0; index--)
+                limits[index] = count *= (seq.Length - index);
+            count -= limits[1];   // to not generate combs with 0 as the first digit
+
+            long sums = 0;
+            for (int position = 0; position < count; position++)
+            {
+                List<int> numbers = seq.ToList();
+                int[] permutation = new int[seq.Length];
+                int index_permutation = 0;
+                long number = 0;
+                bool isDivisible = true;
+                while (isDivisible && index_permutation + 1 < limits.Length)
+                {
+                    int index_numbers = (position % limits[index_permutation]) / limits[index_permutation + 1];
+                    permutation[index_permutation++] = numbers[index_numbers];
+                    numbers.RemoveAt(index_numbers);
+                    //
+                    if (index_permutation > 3)
+                        if (!(isDivisible &= IsDivisible(permutation, index_permutation, primes)))
+                            position += limits[index_permutation] - 1;   // to jump all the combs with the same digits found to not be divisible
+                    number = number * 10 + permutation[index_permutation - 1];
+                }
+                if (isDivisible)
+                {
+                    permutation[index_permutation] = numbers[position % limits[index_permutation++]];
+                    if (isDivisible &= IsDivisible(permutation, index_permutation, primes))
+                        sums += number * 10 + permutation[index_permutation - 1];
+                }
+            }
+
+            return sums;
+        }
+
+        private static bool IsDivisible(int[] permutation, int index_permutation, int[] primes)
+        {
+            int sum = 0;
+            for (int index = 0; index < 3; index++)
+                sum = sum * 10 + permutation[index_permutation - 3 + index];
+            return sum % primes[index_permutation - 4] == 0;
+        }
     }
 }
---
+++ Utility/PanDigital.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Euler.Utility
+{
+    internal static class PanDigital
+    {
+        public static IEnumerable<long> GetAll()
+        {
+            for (long i0 = 1; i0 <= 9; i0++)
+            {
+                for (long i1 = 0; i1 <= 9; i1++)
+                {
+                    if (i1 == i0) continue;
+                    for (long i2 = 0; i2 <= 9; i2++)
+                    {
+                        if (i2 == i0 || i2 == i1) continue;
+                        for (long i3 = 0; i3 <= 9; i3++)
+                        {
+                            if (i3 == i0 || i3 == i1 || i3 == i2) continue;
+                            for (long i4 = 0; i4 <= 9; i4++)
+                            {
+                                if (i4 == i0 || i4 == i1 || i4 == i2 || i4 == i3) continue;
+                                for (long i5 = 0; i5 <= 9; i5++)
+                                {
+                                    if (i5 == i0 || i5 == i1 || i5 == i2 || i5 == i3 || i5 == i4) continue;
+                                    for (long i6 = 0; i6 <= 9; i6++)
+                                    {
+                                        if (i6 == i0 || i6 == i1 || i6 == i2 || i6 == i3 || i6 == i4 || i6 == i5)
+                                            continue;
+                                        for (long i7 = 0; i7 <= 9; i7++)
+                                        {
+                                            if (i7 == i0 || i7 == i1 || i7 == i2 || i7 == i3 || i7 == i4 || i7 == i5 ||
+                                                i7 == i6) continue;
+                                            for (long i8 = 0; i8 <= 9; i8++)
+                                            {
+                                                if (i8 == i0 || i8 == i1 || i8 == i2 || i8 == i3 || i8 == i4 || i8 == i5 ||
+                                                    i8 == i6 || i8 == i7) continue;
+                                                for (long i9 = 0; i9 <= 9; i9++)
+                                                {
+                                                    if (i9 == i0 || i9 == i1 || i9 == i2 || i9 == i3 || i9 == i4 ||
+                                                        i9 == i5 || i9 == i6 || i9 == i7 || i9 == i8) continue;
+
+                                                    yield return i9 + i8 * 10 + i7 * 100 + i6 * 1000 + i5 * 10000 + i4 * 100000 +
+                                                        i3 * 1000000 + i2 * 10000000 + i1 * 100000000 + i0 * 1000000000;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
